8724186d43ec0155e6dea174cea21723fbdc212b
YuraMelnyk <melnyk.yra27@gmail.com>
2019-11-05 21:07:49 +0200

    CRUD elements added

---
+++ WebStoreUI/Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Domain.Abstract;
+
+namespace WebStoreUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        IBookRepository repository;
+
+        public AdminController(IBookRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult Index()
+        {
+            return View(repository.Books);
+        }
+    }
+}
